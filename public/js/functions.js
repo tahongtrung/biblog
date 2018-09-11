f24f92301c5a297b7260dc750432374505ee679f
trung2nd <trung2nd@gmail.com>
2018-09-11 10:41:13 +0700

    update bilog 10:41:13

--- public/js/functions.js
+++ public/js/functions.js
@@ -10,3 +10,32 @@ function scrollFunction() {
 function topFunction() {
     $('html, body').animate({scrollTop:0}, 'slow');
 }
+
+
+let bgImgs = [
+    'https://i.imgur.com/lVDs2eF.png',
+    'https://i.imgur.com/xwvDSTC.png',
+    'https://i.imgur.com/h4k46HM.png',
+    'https://i.imgur.com/A1QPlWy.png'
+];
+function timeOfDay() {
+    let hour = new Date().getHours();
+    if (hour >= 4 && hour <= 11){
+        //return 'morning';
+        return bgImgs[0];
+    }
+    if (hour >= 12 && hour <= 16) {
+        //return 'afternoon';
+        return bgImgs[1];
+    }
+    if (hour >= 17 && hour <= 20) {
+        //return 'evening';
+        return bgImgs[2];
+    }
+    if (hour >= 21 || hour <= 3) {
+        //return 'night';
+        return bgImgs[3];
+    }
+}
+//console.log(`Good ${timeOfDay()}!`);
+document.body.style.background = "#fff url(" + timeOfDay() + ")";
