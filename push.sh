58596b179eebc563074ea5d0633ef23c0488156a
trungth <trungth@hdwebsoft>
2018-09-13 18:10:36 +0700

    update bilog $now $day $DATE

--- push.sh
+++ push.sh
@@ -1,3 +1,22 @@
 now=$(date +"%T")
+day=$(date -d "$date1" +%A)
+DATE=$(date +%F)
+#`date +%Y-%m-%d`
+git add . && git commit -m 'update bilog $now $day $DATE' && git push origin master
 
-git add . && git commit -m 'update bilog $now date -d "$date1" +%A' && git push origin master
+# day=$(date -d "$date1" +%A)
+# echo $day #thursday
+
+# RT=$(date +%F)
+# echo $RT
+
+# DATE=$(date +%-d)
+# echo $DATE
+# M=$(date +%-m)
+# echo $M
+# Y=$(date +%-Y)
+# echo $Y
+# DOW=$(date +%u)
+# echo $DOW
+# DATE=`date +%Y-%m-%d`
+# echo $DATE
