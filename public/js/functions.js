5a9529df97bfe40bc2fb16c6bc7ae37f72f2e737
TrungTH <dev@trung.ml>
2019-01-06 23:50:35 +0700

    update bilog 23:50:34 Sunday 2019-01-06

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,16 +1,3 @@
-function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("content-tab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " is-active";
-  }
 /* scroll button */
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
